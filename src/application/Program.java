57c02f40e53000a5762149ad9966e699d1b9d503
britolmbs <britolmbs@hotmail.com>
2022-05-12 13:49:29 -0300

    Inicio Projeto Xadrez

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessMatch;
+
+public class Program {
+    public static void main(String[] args) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+}
