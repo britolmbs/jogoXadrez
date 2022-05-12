a21ae5824ffa9cbecd8588e96f45c0d738dcdda4
britolmbs <britolmbs@hotmail.com>
2022-05-12 15:53:46 -0300

    Criando peças e exceção

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Erro ao criar o tabuleiro: deve haver pelo menos 1 linha e 1 coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -16,22 +19,39 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column){
+        if (!positionExists(row, column)) {
+            throw new BoardException("Posição não no tabuleiro");
+        }
         return pieces[row][column];
     }
     public Piece piece(Position position){
+        if (!positionExists(position)) {
+            throw new BoardException("Posição não no tabuleiro");
+        }
         return  pieces[position.getRow()][position.getColumn()];
+    }
+    public  void placePiece(Piece piece, Position position){
+        if (thereIsAPiece(position)) {
+            throw new BoardException("Já existe uma peça na posição " + position);
+        }
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+    private boolean positionExists(int row, int column){
+       return row >=0 && row < rows && column >= 0 && column < columns;
+    }
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+    public boolean thereIsAPiece(Position position){
+        if (!positionExists(position)) {
+            throw new BoardException("Posição não no tabuleiro");
+        }
+        return piece(position) != null;
     }
 }
