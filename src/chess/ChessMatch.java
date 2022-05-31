32f40652beae583be558c830c0706d2eec9710c2
britolmbs <britolmbs@hotmail.com>
2022-05-31 16:13:43 -0300

    Jogada especial en Passant FINALIZADA

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -129,6 +129,23 @@ public boolean getCheck(){
             rook.increaseMoveCount();
         }
 
+        // #specialmove en passant
+
+        if(p instanceof Pawn) {
+            if (source.getColumn() != target.getColumn() && capturedPiece == null){
+                Position pawnPosition;
+                if (p.getColor() == Color.WHITE){
+                pawnPosition = new Position(target.getRow() + 1, target.getColumn());
+                }
+                else {
+                    pawnPosition = new Position(target.getRow() - 1, target.getColumn());
+                }
+                capturedPiece = board.removePiece(pawnPosition);
+                capturedPieces.add(capturedPiece);
+                piecesOnTheBoard.remove(capturedPiece);
+            }
+        }
+
         return capturedPiece;
     }
 
@@ -160,6 +177,22 @@ public boolean getCheck(){
             ChessPiece rook = (ChessPiece) board.removePiece(targetT);
             board.placePiece(rook, sourceT);
             rook.increaseMoveCount();
+        }
+
+            //specialmove en passant
+        if(p instanceof Pawn) {
+            if (source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
+                ChessPiece pawn = (ChessPiece)board.removePiece(target);
+                Position pawnPosition;
+                if (p.getColor() == Color.WHITE){
+                    pawnPosition = new Position(3, target.getColumn());
+                }
+                else {
+                    pawnPosition = new Position(4, target.getColumn());
+                }
+                board.placePiece(pawn, pawnPosition);
+
+        }
         }
     }
 
